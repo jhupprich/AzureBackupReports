c830f913e863ec2a95fa29faccbfc642bab8b49f
jhupprich <jhupprich@gmail.com>
2019-02-19 15:49:35 -0900

    fksjfdl

--- Get-MARSBackupReport.ps1
+++ Get-MARSBackupReport.ps1
@@ -19,12 +19,24 @@
 $client = 'ABIN'
 $admin = "admin@nsedc.microsoftonline.com" 
 $securePass = "$@!M0nTaGG1ng*" | ConvertTo-SecureString -AsPlainText -Force
-$subID
+$subID = "d6a3f26f-407a-49ce-b0ec-97f6b7109d6f"
+$vault = "NSEDC-File"
 
 #static variables
+$cred = New-Object -TypeName System.Management.Automation.PSCredential `
+-argumentlist $admin,$securePass
 $arr01 = @()
 $arr02 = @()
 $date = Get-Date -Format MM-dd-yyyy
 $path = "C:\scripts\" + $client + "_BackupReport_" + $date + ".xlsx"
 $subjectPass = "[SUCCESS] - " + $client + " Recovery Point Report for " + $date
 $subjectFail = "[FAILURE] - " + $client + " Recovery Point Report for " + $date
+
+
+#connect and fetch data
+Login-AzureRmAccount -Credential $cred -Subscription $subID
+
+$vault = Get-AzureRmRecoveryServicesVault -name $vault
+Set-AzureRmRecoveryServicesVaultContext -Vault $vault
+
+
